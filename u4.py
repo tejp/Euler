d1bc0d79e88338c4ced0334a0ee1c3daaa4dedba
tejp <cyberkygen@gmail.com>
2015-02-23 21:42:58 +0100

    add u4

---
+++ u4.py
@@ -0,0 +1,26 @@
+import timeit
+
+
+def isPal(n):
+    nbr = str(n)
+    return nbr == nbr[::-1]
+
+
+def getPal(i=999, p=False):
+    highPal = -1
+    lowFactor = int(i/10)
+    while i > lowFactor:
+        for j in range(i, lowFactor, -1):
+            n = i*j
+            if not n > highPal:
+                break
+            elif isPal(n):
+                highPal, lowFactor = n, j
+                if p:
+                    print("{}*{} = {}".format(i, j, n))
+                break
+        i -= 1
+    return highPal
+
+getPal(p=True)
+print(timeit.timeit(getPal, number=1000))
